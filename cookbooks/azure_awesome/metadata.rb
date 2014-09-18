c3fd3c80bc90ef44f1a4575e40dfe1edb79969cf
Matt Stratton <matt.stratton@gmail.com>
2014-09-18 14:31:10 -0500

    Add installer for TeamViewer

--- cookbooks/azure_awesome/metadata.rb
+++ cookbooks/azure_awesome/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mattstratton@getchef.com'
 license          'Apache 2.0'
 description      'Installs/Configures azure_awesome'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 supports 'windows'
 
 depends 'windows', '~> 1.34.2'
---
+++ cookbooks/azure_awesome/recipes/teamviewer.rb
@@ -0,0 +1,11 @@
+cookbook_file 'c:/TeamViewer_Setup_en.exe' do
+  source 'TeamViewer_Setup_en.exe'
+end
+
+
+windows_package 'TeamViewer 9' do
+  source 'c:/TeamViewer_Setup_en.exe'
+  options "/S"
+  installer_type :custom
+  action :install
+end
