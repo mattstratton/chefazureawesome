ac0c11675e6595e70fb356ea5e491e15a75a3982
Matt Stratton <matt.stratton@gmail.com>
2014-10-21 11:53:25 -0500

    add ruby dependency

--- cookbooks/myface/metadata.rb
+++ cookbooks/myface/metadata.rb
@@ -4,12 +4,13 @@ maintainer_email "someara@opscode.com"
 license          "Apache 2"
 description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.2"
 
 depends "mysql"
 depends "database"
 depends "apache2"
 depends "php"
+depends "ruby"
 
 
 
