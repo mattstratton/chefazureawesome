fd12e6151eb7abf8313addc027fa3916f2634280
Matt Stratton <matt.stratton@gmail.com>
2014-10-21 11:02:13 -0500

    update with kitchen

--- cookbooks/azure-lamp/metadata.rb
+++ cookbooks/azure-lamp/metadata.rb
@@ -1,7 +1,9 @@
 name             'azure-lamp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures azure-lamp'
+maintainer       'Matt Stratton'
+maintainer_email 'mattstratton@getchef.com'
+license          'Apache 2.0'
+description      'Installs/Configures LAMP stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apt'
