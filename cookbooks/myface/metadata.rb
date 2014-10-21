3b8a4aa3ec22595ad838717994eaa5b30f2eee41
Matt Stratton <matt.stratton@gmail.com>
2014-10-21 11:44:56 -0500

    update for supermarket

--- cookbooks/myface/metadata.rb
+++ cookbooks/myface/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "someara@opscode.com"
 license          "Apache 2"
 description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.0"
+version          "0.3.1"
 
 depends "mysql"
 depends "database"
